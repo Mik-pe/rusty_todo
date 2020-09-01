c1050cd13116f29f86eac45b917475f3ce1f2e8d
Mikael Pettersson <mikael.pettersson@liu.se>
2020-09-01 07:55:07 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,86 @@
+use cursive::traits::*;
+use cursive::views::{Button, Dialog, DummyView, EditView, LinearLayout, SelectView};
+use cursive::Cursive;
+
+fn main() {
+    let mut siv = cursive::default();
+
+    let select = SelectView::<String>::new()
+        .on_submit(on_submit)
+        .with_name("select")
+        .fixed_size((20, 12));
+    let buttons = LinearLayout::vertical()
+        .child(Button::new("Add new", add_name))
+        .child(Button::new("Delete", delete_name))
+        .child(DummyView)
+        .child(Button::new("Quit", Cursive::quit));
+
+    siv.add_layer(
+        Dialog::around(
+            LinearLayout::horizontal()
+                .child(select)
+                .child(DummyView)
+                .child(buttons),
+        )
+        .title("Morning meeting"),
+    );
+
+    siv.run();
+}
+
+fn add_name(s: &mut Cursive) {
+    fn ok(s: &mut Cursive, name: &str) {
+        let formatted_name = format!("[ ] - {}", name);
+        s.call_on_name("select", |view: &mut SelectView<String>| {
+            view.add_item_str(&formatted_name)
+        });
+        s.pop_layer();
+    }
+
+    s.add_layer(
+        Dialog::around(
+            EditView::new()
+                .on_submit(ok)
+                .with_name("name")
+                .fixed_width(10),
+        )
+        .title("Enter a new name")
+        .button("Ok", |s| {
+            let name = s
+                .call_on_name("name", |view: &mut EditView| view.get_content())
+                .unwrap();
+            ok(s, &name);
+        })
+        .button("Cancel", |s| {
+            s.pop_layer();
+        }),
+    );
+}
+
+fn delete_name(s: &mut Cursive) {
+    let mut select = s.find_name::<SelectView<String>>("select").unwrap();
+    match select.selected_id() {
+        None => s.add_layer(Dialog::info("No name to remove")),
+        Some(focus) => {
+            select.remove_item(focus);
+        }
+    }
+}
+
+fn on_submit(s: &mut Cursive, name: &str) {
+    let mut select = s.find_name::<SelectView<String>>("select").unwrap();
+    match select.selected_id() {
+        None => s.add_layer(Dialog::info("No name to remove")),
+        Some(focus) => {
+            select.remove_item(focus);
+            let mut new_string = String::from(name);
+            let start = name.find("X");
+            if start.is_none() {
+                new_string = new_string.replace("[ ]", "[X]");
+            } else {
+                new_string = new_string.replace("[X]", "[ ]");
+            }
+            select.insert_item_str(focus, &new_string)
+        }
+    }
+}
