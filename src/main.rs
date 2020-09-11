faf9d769d0ad53a9d533571414617f20a3c9ad69
Mikael Pettersson <mikael.pettersson@liu.se>
2020-09-11 09:44:38 +0200

    Added emojis 🔈!

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use cursive::traits::*;
 use cursive::views::{Button, Dialog, DummyView, EditView, LinearLayout, SelectView};
 use cursive::Cursive;
-use std::time::SystemTime;
-
-use chrono::prelude::*;
 
 fn main() {
     let mut siv = cursive::default();
@@ -28,7 +25,7 @@ fn main() {
         .title("Morning meeting")
         .with_name("main_dialog"),
     );
-
+    siv.add_global_callback('c', on_clear);
     siv.refresh();
     while siv.is_running() {
         // let date = Local::now();
@@ -41,7 +38,9 @@ fn main() {
 
 fn add_name(s: &mut Cursive) {
     fn ok(s: &mut Cursive, name: &str) {
-        let formatted_name = format!("[ ] - {}", name);
+        //For some reason the checkmark removes the last character...
+        //workaround by adding a space last
+        let formatted_name = format!("[🔈] - {} ", name);
         let mut num_attendees = 0usize;
         s.call_on_name("select", |view: &mut SelectView<String>| {
             view.add_item_str(&formatted_name);
@@ -87,19 +86,39 @@ fn delete_name(s: &mut Cursive) {
     });
 }
 
+fn on_clear(s: &mut Cursive) {
+    let mut select = s.find_name::<SelectView<String>>("select").unwrap();
+    match select.selected_id() {
+        None => s.add_layer(Dialog::info("No name to remove")),
+        Some(focus) => {
+            let name = {
+                let (_, name) = select.get_item(focus).unwrap();
+                name.clone()
+            };
+            select.remove_item(focus);
+            let mut new_string = String::from(name);
+            new_string = new_string.replace("[✔️]", "[🔈]");
+            new_string = new_string.replace("[🔊]", "[🔈]");
+            select.insert_item_str(focus, &new_string);
+            select.set_selection(focus);
+            ()
+        }
+    }
+}
+
 fn on_submit(s: &mut Cursive, name: &str) {
-    s.screen_mut();
     let mut select = s.find_name::<SelectView<String>>("select").unwrap();
     match select.selected_id() {
         None => s.add_layer(Dialog::info("No name to remove")),
         Some(focus) => {
             select.remove_item(focus);
             let mut new_string = String::from(name);
-            let start = name.find("X");
-            if start.is_none() {
-                new_string = new_string.replace("[ ]", "[X]");
+            if name.find("[✔️]").is_some() {
+                new_string = new_string.replace("[✔️]", "[🔈]");
+            } else if name.find("[🔊]").is_some() {
+                new_string = new_string.replace("[🔊]", "[✔️]");
             } else {
-                new_string = new_string.replace("[X]", "[ ]");
+                new_string = new_string.replace("[🔈]", "[🔊]");
             }
             select.insert_item_str(focus, &new_string);
             select.set_selection(focus);
